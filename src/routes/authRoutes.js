dafa14101e053b663679678e64e7bff641d9b8c4
Anh Viet <anhviet5121pc@gmail.com>
2021-04-17 17:07:51 +0700

    Clean some code

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -12,33 +12,33 @@ router.post("/signup", (req, res) => {
   db.getConnection(function (err, connection) {
     if (err) throw err;
     console.log("MySQL connected!");
-    // Check username is already existed or not
-    var selectSql = `SELECT username FROM Users WHERE username = '${username}'`;
 
-    connection.query(selectSql, function (err, result) {
-      // If not exist
-      if (_.isEmpty(result)) {
-        // Insert to database
-        var insertSql = `INSERT INTO Users (username, password, fullname) VALUES ('${username}', '${password}', '${fullname}')`;
+    try {
+      // Check username is already existed or not
+      var selectSql = `SELECT username FROM Users WHERE username = '${username}'`;
 
-        connection.query(insertSql, function (err, result) {
-          // Done with sign up
-          console.log("An account is registered");
-          connection.release();
-          if (err) throw err;
-          return res.status(200).send("Created");
-        });
-      } else {
-        // There is a similar username exists
-        console.log("There is already this username");
-        connection.release();
-        if (err) throw err;
-        return res.status(422).send("username exists");
-      }
-    })
+      connection.query(selectSql, function (err, result) {
+        // If not exist
+        if (_.isEmpty(result)) {
+          // Insert to database
+          var insertSql = `INSERT INTO Users (username, password, fullname) VALUES ('${username}', '${password}', '${fullname}')`;
+
+          connection.query(insertSql, function (err, result) {
+            // Done with sign up
+            console.log("An account is registered");
+            res.status(200).send("Created");
+          });
+        } else {
+          // There is a similar username exists
+          console.log("There is already this username");
+          res.status(422).send("username exists");
+        }
+      })
+    } finally {
+      if (err) throw err;
+      connection.release();
+    }
   });
-
-  //res.send("You made a post request");
 });
 
 module.exports = router;
