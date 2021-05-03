90c8ec0e35816c1e31b52243967a189745c13ce1
Anh Viet <anhviet5121pc@gmail.com>
2021-05-04 00:15:30 +0700

    Update add PersonalTasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const personalRoutes = require("./routes/personalRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(personalRoutes);
 
 const PORT = 3000;
 
---
+++ src/routes/personalRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const pool = require("../database/mysql");
+
+const _ = require("lodash");
+
+const router = express.Router();
+router.post("/personal/add", (req, res) => {
+  const {
+    PTID,
+    title,
+    details,
+    startDate,
+    startTime,
+    finishDate,
+    finishTime,
+    status,
+    username,
+  } = req.body;
+
+  // Check id in db
+  pool.query(
+    `SELECT PTID From PersonalTasks WHERE PTID = '${PTID}'`,
+    function (err, rows) {
+      if (err) throw err;
+      if (_.isEmpty(rows)) {
+        pool.query(
+          `INSERT INTO PersonalTasks (PTID, title, details, startDate, startTime, finishDate, finishTime, status, owner) VALUES ('${PTID}', '${title}', '${details}','${startDate}', '${startTime}', '${finishDate}', '${finishTime}', '${status}', '${username}')`,
+          function (err, rows) {
+            if (err) throw err;
+            console.log("A personal task is added");
+            res.status(200).send("Add personal task successfully");
+          }
+        );
+      } else {
+        console.log("There is already this personal task id");
+        res.status(400).send("PTID exists");
+      }
+    }
+  );
+});
+
+module.exports = router;
