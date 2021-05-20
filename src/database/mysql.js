a9083fc89d2b701dde502541a18f898b6cd97929
Anh Viet <anhviet5121pc@gmail.com>
2021-05-21 02:37:35 +0700

    Forget heroku

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "AndroidTaskMonitoring",
+  host: "us-cdbr-east-03.cleardb.com",
+  user: "b8874d5b429249",
+  password: "169a02fb",
+  database: "heroku_7704eb513bc578f",
 });
 
 module.exports = pool;
